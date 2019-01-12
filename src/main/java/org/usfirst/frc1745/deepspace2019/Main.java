3b2c78fd2ba8ad37a49b978e9724cc79637d3670
Kusha Gharahi <3326002+kushagharahi@users.noreply.github.com>
2019-01-12 21:18:00 +0000

    Add build badge (#1)
    * add build badge
    
    * getting build to work

--- src/main/java/org/usfirst/frc1745/deepspace2019/Main.java
+++ src/main/java/org/usfirst/frc1745/deepspace2019/Main.java
@@ -9,7 +9,7 @@
 // it from being updated in the future.
 
 
-package org.usfirst.frc1745.deep-space-2019;
+package org.usfirst.frc1745.deepspace2019;
 
 import edu.wpi.first.wpilibj.RobotBase;
 
--- src/main/java/org/usfirst/frc1745/deepspace2019/Robot.java
+++ src/main/java/org/usfirst/frc1745/deepspace2019/Robot.java
@@ -9,7 +9,7 @@
 // it from being updated in the future.
 
 
-package org.usfirst.frc1745.deep-space-2019;
+package org.usfirst.frc1745.deepspace2019;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.command.Command;
@@ -17,8 +17,7 @@ import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import org.usfirst.frc1745.deep-space-2019.commands.*;
-import org.usfirst.frc1745.deep-space-2019.subsystems.*;
+import org.usfirst.frc1745.deepspace2019.commands.*;
 
 /**
  * The VM is configured to automatically run this class, and to call the
