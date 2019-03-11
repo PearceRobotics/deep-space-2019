34d0610f0f65270b5e91acf1dd5daf4c807612c9
Kusha Gharahi <3326002+kushagharahi@users.noreply.github.com>
2019-03-10 23:47:57 -0500

    remove junk

--- src/main/java/org/usfirst/frc1745/deepspace2019/subsystems/LedPWMController.java
+++ src/main/java/org/usfirst/frc1745/deepspace2019/subsystems/LedPWMController.java
@@ -1,17 +1,8 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
 
 package org.usfirst.frc1745.deepspace2019.subsystems;
 
 import edu.wpi.first.wpilibj.Spark;
 
-/**
- * Add your docs here.
- */
 public class LedPWMController {
     private Spark blinkin;
 
--- src/main/java/org/usfirst/frc1745/deepspace2019/subsystems/vision/Vision.java
+++ src/main/java/org/usfirst/frc1745/deepspace2019/subsystems/vision/Vision.java
@@ -1,9 +1,3 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
 
 package org.usfirst.frc1745.deepspace2019.subsystems.vision;
 
@@ -11,9 +5,6 @@ import org.usfirst.frc1745.deepspace2019.subsystems.LedPWMController;
 import org.usfirst.frc1745.deepspace2019.subsystems.LedPWMController.BlinkinColors;
 import org.usfirst.frc1745.deepspace2019.subsystems.drive.DrivingDeltas;
 
-/**
- * Add your docs here.
- */
 public class Vision {
     private Limelight limelight;
     private Ultrasonic ultrasonicLeft;
